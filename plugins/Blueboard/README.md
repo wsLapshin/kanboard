cf6619cdb703c84343db5833af6671c58c7a6727
Lapshin Sergey <wslapshin@gmail.com>
2018-08-04 07:35:41 +0300

    encancements

---
+++ plugins/Blueboard/README.md
@@ -0,0 +1,10 @@
+Simple CSS Theme for [kanboard](https://kanboard.org/).
+
+Download zip Archive or clone in plugins folder of you Kanboard installation.
+
+Not so clean but do the job.
+
+![](https://raw.githubusercontent.com/bgibout/blueboard/master/screenshot.png)
+
+Enjoy :)
+
---
+++ plugins/TimerOverview/Controller/TimerOverviewController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Kanboard\Plugin\TimerOverview\Controller;
+
+use Kanboard\Controller\BaseController;
+
+/**
+ * TimerOverview
+ *
+ * @package controller
+ * @author  Martin Middeke
+ */
+class TimeroverviewController extends BaseController
+{
+    /**
+     * TimerOverview index page
+     *
+     * @access public
+     */
+    public function index()
+    {
+        $user = $this->getUser();
+
+        $this->response->html($this->helper->layout->dashboard('dashboard/subtasks', array(
+            'title' => t('Running timers for %s', $this->helper->user->getFullname($user)),
+            'paginator' => $this->subtaskPagination->getDashboardPaginator($user['id'], 'subtasks', 50),
+            'user' => $user,
+            'nb_subtasks' => $this->subtaskModel->countByAssigneeAndTaskStatus($user['id']),
+        )));
+
+
+        /*$this->response->html($this->helper->layout->dashboard('dashboard/subtasks', array(
+            'title' => t('Subtasks overview for %s', $this->helper->user->getFullname($user)),
+            'paginator' => $this->subtaskPagination->getDashboardPaginator($user['id']),
+            'user' => $user,
+            'nb_subtasks' => $this->subtaskModel->countByAssigneeAndTaskStatus($user['id']),
+        )));*/
+    }
+}
